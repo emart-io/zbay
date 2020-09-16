37566a50db16403951fa10c3a9101989a858ed65
Daniel <jmzwcn@gmail.com>
2020-09-16 20:41:36 +0800

    Support grpc-web in-process.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- go.mod
+++ go.mod
@@ -1,9 +1,15 @@
 module github.com/emart.io/zbay
 
 require (
+	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/gogo/protobuf v1.2.1
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/improbable-eng/grpc-web v0.13.0
+	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
+	github.com/rs/cors v1.7.0 // indirect
 	github.com/sirupsen/logrus v1.4.2
+	golang.org/x/net v0.0.0-20190311183353-d8887717615a
 	google.golang.org/grpc v1.20.1
 )
 
--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"net"
+	"net/http"
 
 	impl "github.com/emart.io/zbay/internal/impl/service"
 	pb "github.com/emart.io/zbay/service/go"
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 )
@@ -31,8 +33,34 @@ func main() {
 	pb.RegisterCommentsServer(s, &impl.CommentImpl{})
 	pb.RegisterMemosServer(s, &impl.MemoImpl{})
 
+	wrappedServer := http.Server{
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.ProtoMajor != 2 {
+				w.Header().Set("Access-Control-Allow-Origin", "*")
+				w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+				w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-User-Agent, X-Grpc-Web,authorization-token")
+				w.Header().Set("grpc-status", "")
+				w.Header().Set("grpc-message", "")
+			}
+			grpcweb.WrapServer(s).ServeHTTP(w, r)
+		}),
+	}
+
+	// wrappedServer := &http.Server{
+	// 	Handler: h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 		if r.ProtoMajor != 2 {
+	// 			w.Header().Set("Access-Control-Allow-Origin", "*")
+	// 			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	// 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-User-Agent, X-Grpc-Web,authorization-token")
+	// 			w.Header().Set("grpc-status", "")
+	// 			w.Header().Set("grpc-message", "")
+	// 		}
+	// 		grpcweb.WrapServer(s).ServeHTTP(w, r)
+	// 	}), &http2.Server{}),
+	// }
+
 	log.Infoln("listen:" + port)
-	if err := s.Serve(lis); err != nil {
+	if err := wrappedServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
